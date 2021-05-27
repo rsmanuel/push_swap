81135d57591e01262d49f7d9801f2095cc151376
Rodrigo <r.silvamanuel42@gmail.com>
2021-05-27 16:52:09 +0100

    idk what i'm doing

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -1,13 +1,5 @@
-#include <stdio.h>
+#include "includes/push_swap.h"
 
 int main(int ac, char **av)
 {
-    char	stack_a[1];
-	int i;
-	int j;
-
-	i = 1;
-	j = 0;
-	av[1] = &stack_a[0];
-	printf("%c", stack_a[0]);
-}
+}	
