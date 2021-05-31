a23b1bd45da939a75ba025ed559e035561e80a4a
Rodrigo <r.silvamanuel42@gmail.com>
2021-05-31 22:06:25 +0100

    😤

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -1,5 +1,43 @@
 #include "includes/push_swap.h"
+#include <stdio.h>
+
+void print(t_stack *head)
+{
+	while (head)
+	{
+		printf("| %d | \n", head->data);
+		head = head->next;
+	}
+}
+/* Função para inserir a node no inicio
+da linked list */
+void stack(t_stack **head, int data)
+{
+	t_stack *new;
+	
+	new = (t_stack *)malloc(sizeof(t_stack));
+	new->data = data;
+	new->prev = NULL;
+	new->next = (*head);
+	(*head) = new; 
+}
 
 int main(int ac, char **av)
 {
+	t_stack *data;
+	int count;
+
+	data = NULL;
+	count = 0;
+    if (ac > 1)
+	{
+		while(*++av)
+		{
+			stack(&data , ft_atoi(*av));
+			count++;
+		}
+		print(data);
+	}
+	else
+		return (1);
 }	
--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,4 +3,11 @@
 
 # include "../libft/libft.h"
 
+typedef struct  s_stack
+{
+    int data;
+    struct s_stack *next;
+    struct s_stack *prev;
+} 				t_stack;
+
 #endif
